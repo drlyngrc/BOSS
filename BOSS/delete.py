c209c1f8b2a343d779e9ccd3b9c03b1e98e565cf
hishiro <123607245+drlyngrc@users.noreply.github.com>
2023-12-07 09:46:29 +0800

    UPDATED

--- BOSS/delete.py
+++ BOSS/delete.py
@@ -5,6 +5,10 @@ class Delete:
         self.db = db_connector
 
     def delete_residentinfo(self, barangayid):
+        # Check for related records and delete them first
+        self.delete_related_records(barangayid)
+
+        # Now delete the main residentinfo
         query = "DELETE FROM residentinfo WHERE BarangayID = %s"
         value = (barangayid,)
         self.db.execute_query(query, value)
@@ -16,9 +20,21 @@ class Delete:
         self.db.execute_query(query, value)
         self.db.commit()
 
+    def del_medicalinfo(self, barangayid):
+        query = "DELETE FROM medicalinfo WHERE BarangayID = %s"
+        value = (barangayid,)
+        self.db.execute_query(query, value)
+        self.db.commit()
+
     def delete_caseinfo(self, caseid):
         query = "DELETE FROM caseinfo WHERE CaseID = %s"
         value = (caseid,)
+        self.db.execute_query(query, value)
+        self.db.commit()
+
+    def del_caseinfo(self, barangayid):
+        query = "DELETE FROM caseinfo WHERE BID = %s"
+        value = (barangayid,)
         self.db.execute_query(query, value)
         self.db.commit()
 
@@ -28,9 +44,21 @@ class Delete:
         self.db.execute_query(query, value)
         self.db.commit()
 
+    def del_addressinfo(self, barangayid):
+        query = "DELETE FROm addressinfo WHERE BarangayID = %s"
+        value = (barangayid,)
+        self.db.execute_query(query, value)
+        self.db.commit()
+
     def delete_adultinfo(self, adultid):
         query = "DELETE FROM adultinfo WHERE EmployeeID = %s"
         value = (adultid,)
+        self.db.execute_query(query, value)
+        self.db.commit()
+
+    def del_adultinfo(self, barangayid):
+        query = "DELETE FROM adultinfo WHERE BarangayID = %s"
+        value = (barangayid,)
         self.db.execute_query(query, value)
         self.db.commit()
 
@@ -40,10 +68,37 @@ class Delete:
         self.db.execute_query(query, value)
         self.db.commit()
 
+    def del_studentinfo(self, barangayid):
+        query = "DELETE FROM studentinfo WHERE BarangayID = %s"
+        value = (barangayid,)
+        self.db.execute_query(query, value)
+        self.db.commit()
+
     def delete_appointment(self, appointmentid):
         query = "DELETE FROM appointment WHERE appointmentid = %s"
         value = (appointmentid,)
         self.db.execute_query(query, value)
         self.db.commit()
 
+    def del_appointment(self, barangayid):
+        query = "DELETE FROM appointment WHERE BarangayID = %s"
+        value = (barangayid,)
+        self.db.execute_query(query, value)
+        self.db.commit()
     
+    def del_logininfo(self, barangayid):
+        query = "DELETE FROM logininfo WHERE BarangayID = %s"
+        value = (barangayid,)
+        self.db.execute_query(query, value)
+        self.db.commit()
+
+    def delete_related_records(self, barangayid):
+        # Delete related records in other tables
+        self.del_medicalinfo(barangayid)
+        self.del_caseinfo(barangayid)
+        self.del_addressinfo(barangayid)
+        self.del_adultinfo(barangayid)
+        self.del_studentinfo(barangayid)
+        self.del_appointment(barangayid)
+        self.del_logininfo(barangayid)
+        # ... delete from other related tables
