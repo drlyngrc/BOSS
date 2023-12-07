755a48781a92a217da653699ec3d49d03c0ad3d3
hishiro <123607245+drlyngrc@users.noreply.github.com>
2023-12-07 17:32:46 +0800

    Add files via upload

---
+++ BOSS_SRC/package/appointment.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+class Appointment:
+    def __init__(self, read_instance, delete_instance, update_instance, resident_updater):
+        self.read_instance = read_instance
+        self.delete_instance = delete_instance
+        self.update_instance = update_instance
+        self.resident_updater = resident_updater
+
+class AppointmentManager(Appointment):
+    def appointment(self, request):
+        if request.method == "POST":
+            if "action" in request.form:
+                if request.form["action"] == "delete":
+                    appointmentid = request.form["deleteappointmentid"]
+                    self.delete_instance.delete_appointment(appointmentid)
+                elif request.form["action"] == "edit":
+                    editappointmentid = request.form["editappointmentid"]
+                    editstatus = request.form["editstatus"]
+
+                    return redirect(url_for("updateAppointment", editappointmentid=editappointmentid, editstatus=editstatus))
+
+        appointment = self.read_instance.read_appointment()
+        return render_template("Admin/Appointment/appointment.html", appointment=appointment)
+
+    def updateAppointment(self, request):
+        if request.method == "POST":
+            updateappointmentid = request.form["updateappointmentid"]
+            updatestatus = request.form["updatestatus"]
+    
+            self.resident_updater.update_appointment(updateappointmentid, updatestatus)
+            return redirect("/admin/appointment")
+
+        editappointmentid = request.args.get("editappointmentid")
+        editstatus = request.args.get("editstatus")
+
+        return render_template("Admin/Update/updateAppointment.html", editappointmentid=editappointmentid, editstatus=editstatus)
